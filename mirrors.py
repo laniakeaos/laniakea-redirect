6e1a0458c6e7b9c51a65399dd25917cedb253b52
Aspen Schneider <rendezvous71@outlook.com>
2024-12-22 11:02:35 +0900

    Store IP address with country code.

--- mirrors.py
+++ mirrors.py
@@ -2,6 +2,10 @@ import ipaddress
 
 from ipwhois import IPWhois
 
+
+ip_data = {}
+
+
 FALLBACK_URL = "https://mirror.rackspace.com/archlinux/$repo/os/$arch"
 
 
@@ -26,12 +30,17 @@ def get_country(ip_addr: str):
     country_code = "us"
 
     try:
-        whois = IPWhois(ip_addr)
-        result = whois.lookup_rdap()
-        country_code: str = result.get("asn_country_code", "us")
-        country_code = country_code.lower()
+        country_code = ip_data.get(ip_addr, None)
+
+        if country_code is None:
+            whois = IPWhois(ip_addr)
+            result = whois.lookup_rdap()
+            country_code: str = result.get("asn_country_code", "us")
+            country_code = country_code.lower()
+            # Store.
+            ip_data[ip_addr] = country_code
     except:
-        pass
+        country_code = "us"
 
     if country_code == "ko":
         return countries["jp"]
